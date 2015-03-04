d900ac073c48c1db548573a29488d7705ed764e6
Corey Csuhta <git@cjcsuhta.com>
2015-03-04 00:36:24 -0500

    Futz around with tables

--- app/helpers/admin/internal_helper.rb
+++ app/helpers/admin/internal_helper.rb
@@ -19,4 +19,27 @@ module Admin::InternalHelper
     return content_tag(:p, hint_text, class:"hint")
   end
 
+  def index_th(label, sort:false)
+
+    sorting_class = sort.present?? "-sortable" : "-unsortable"
+    sort_direction = nil
+
+    if label == :id
+      label = "ID"
+    elsif label.is_a?(Symbol)
+      label = label.to_s.humanize.titleize
+    end
+
+    content_tag(:th, label, class:"#{sorting_class} #{sort_direction}")
+
+  end
+
+  def index_td(content, class:false)
+    content_tag :td, content
+  end
+
+  def actions_th
+    index_th("Actions", sort:false)
+  end
+
 end
