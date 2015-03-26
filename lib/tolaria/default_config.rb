cc753b31a67119574ce4c218916f04cd444950fd
Corey Csuhta <git@cjcsuhta.com>
2015-03-26 19:40:16 -0400

    Raise default bcrypt effort

--- lib/tolaria/default_config.rb
+++ lib/tolaria/default_config.rb
@@ -20,7 +20,7 @@ Tolaria.configure do |config|
   # wall clock time bcrypt takes to hash a passphrase.
   # It is VERY DANGEROUS to set this below 10 for production.
   # Use 1 in test mode to speed up your test suite.
-  config.bcrypt_cost = Rails.env.test?? 1 : 10
+  config.bcrypt_cost = Rails.env.test?? 1 : 12
 
   # The length of time that emailed passcodes should be valid.
   # It is STRONGLY RECOMMENDED that you keep this under 30 minutes.
@@ -33,10 +33,6 @@ Tolaria.configure do |config|
   # The length of time that an administrator’s account is disabled
   # after they trip the lockout threshold.
   config.lockout_duration = 1.hour
-
-  # The amount of time that an administrator’s login lasts.
-  # Refreshed every time they return to the site.
-  config.session_length = 3.weeks
 
   # Tolaria attempts to convert models to a pretty “display”
   # string for presenting in forms and listings.
