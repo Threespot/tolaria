124714265b60bc50498bdef1de98ba0541180a5f
Corey Csuhta <git@cjcsuhta.com>
2015-03-28 02:07:31 -0400

    Update initializer generator

--- lib/generators/tolaria/install/templates/tolaria_initializer.rb
+++ lib/generators/tolaria/install/templates/tolaria_initializer.rb
@@ -4,17 +4,17 @@ Tolaria.configure do |config|
   # navigation and HTML page titles.
   config.interface_title = "Company Name"
 
-  # Redirect the administratior to this route by default when logging in.
-  config.default_redirect = :admin_administrators_path
-
-  # The default order to sort items on index pages.
-  # "id DESC" will usually show newest things at the top of the list.
-  # config.default_sort_order = "id DESC"
-
   # Tolaria sends authentication emails. Set the value of the FROM field here.
   # This is NOT a trivial choice, you might have to get this address
   # whitelisted by your target companies.
   config.from_address = "Rails <tolaria@example.org>"
+
+  # Redirect the administratior to this route by default when logging in.
+  config.default_redirect = :admin_administrators
+
+  # The default order to sort items on index pages.
+  # "id DESC" will usually show newest things at the top of the list.
+  # config.default_sort_order = "id DESC"
 
   # The cost factor for bcrypt. Raising this number will increase the
   # wall clock time bcrypt takes to hash a passphrase.
@@ -35,8 +35,8 @@ Tolaria.configure do |config|
   # config.lockout_duration = 1.hour
 
   # Tolaria attempts to convert models to a pretty “display” string for
-  # resenting in forms and listings. You can define `admin_name` on your
-  # model to set the string yourself, or you can append your own
+  # presenting in forms and listings. You can define `admin_name` or `to_s`
+  # on your model to set the string yourself, or you can append your own
   # preferred methods to the front of Tolaria’s list:
   # config.display_name_methods.unshift(:method_name)
 
