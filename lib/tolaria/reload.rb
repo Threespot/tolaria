f7d1319c21c49f73b0f186fc73c8a63558b3d88b
Corey Csuhta <git@cjcsuhta.com>
2015-05-11 11:47:12 -0400

    Go through the "proper" Rails autoload system

--- lib/tolaria/reload.rb
+++ lib/tolaria/reload.rb
@@ -1,17 +1,17 @@
-# Tolaria needs the latest and greatest copies of the models
-# and admin settings at all times. This file is a bit of a hack
-# so that we can trigger a code refresh whenever we need to.
-
 module Tolaria
 
-  def self.reload_app_folder!
+  # To provide admin pane development magic, Tolaria provides this method
+  # to force Rails to autoload/reload all model files.
+  def self.reload_models!
 
-    # KLUDGE: Reference the `Administrator` object so that Rails autoloads
+    # Reference the `Administrator` object so that Rails autoloads
     # it from the engine directory
     Administrator
 
-    Dir["#{Rails.root}/app/models/*.rb", "#{Rails.root}/app/admin/*.rb"].each do |file|
-      load file
+    # Reference each ActiveRecord::Base model so Rails
+    # autoloads or reloads it.
+    Dir["#{Rails.root}/app/models/*.rb"].each do |file|
+      File.basename(file, ".rb").camelize.constantize
     end
 
   end
