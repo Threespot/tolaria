97c824ead4c0e7d64107834f2a3d2a75a25be760
Corey Csuhta <git@cjcsuhta.com>
2015-05-27 14:34:04 -0400

    Force FormBuildable to load

--- lib/tolaria/form_buildable.rb
+++ lib/tolaria/form_buildable.rb
@@ -121,3 +121,13 @@ module Tolaria
 
   end
 end
+
+class Admin::FormBuilder < ActionView::Helpers::FormBuilder
+  include Tolaria::FormBuildable
+  delegate :content_tag, :tag, :render, to: :@template
+end
+
+class Ransack::Helpers::FormBuilder
+  include Tolaria::FormBuildable
+  delegate :content_tag, :tag, :render, to: :@template
+end
