7529b1977bb9ed0f42e7370fbe105217300f7a8a
Corey Csuhta <Csuhta@thanatos.cjcsuhta.com>
2015-06-08 12:21:22 -0400

    Improve router test

--- test/integration/router_test.rb
+++ test/integration/router_test.rb
@@ -2,7 +2,13 @@ require "test_helper"
 
 class RouterTest < ActionDispatch::IntegrationTest
 
-  test "can manage, check, and unmanage a model" do
+  test "draws admin routes" do
+    assert admin_new_session_path
+    assert admin_destroy_session_path
+    assert admin_root_path
+  end
+
+  test "draws automatic model routes" do
 
     class ::Card < ActiveRecord::Base
       manage_with_tolaria using:{
@@ -15,13 +21,21 @@ class RouterTest < ActionDispatch::IntegrationTest
       Tolaria.draw_routes(self)
     end
 
-    # Can we use the routes here and in the demo app?
+    # Test all of the routes Tolaria should have drawn
+    # from here and from the demo application
+
     assert_equal admin_cards_path, "/admin/cards"
     assert_equal admin_card_path(1), "/admin/cards/1"
+
     assert admin_blog_posts_path
     assert admin_blog_posts_path(1)
     assert edit_admin_blog_post_path(1)
     assert admin_blog_post_path(1)
+
+    assert admin_categories_path
+    assert admin_category_path(1)
+    assert edit_admin_category_path(1)
+    assert admin_category_path(1)
 
     # Unseat the Card class so that it doesn't leak out of this test
     assert Tolaria.discard_managed_class(Card)
